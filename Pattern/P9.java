d32bf0ff426f6c9c698fea36b7cecf0f2d0dbcc6
RajShashwat <shashwat.r2018bcs@srisriuniversity.edu.in>
2019-06-06 14:11:52 +0530

    Pattern6
    55555
    44444
    33333
    22222
    11111

---
+++ Pattern/P9.java
@@ -0,0 +1,17 @@
+package Example;
+
+public class Pattern9 {
+
+    public static void main (String[] args) {
+
+        for (char i = 'E'; i >= 'A'; i--){
+
+            for(char j = 'E'; j >= 'A'; j--) {
+
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+    }
+}
